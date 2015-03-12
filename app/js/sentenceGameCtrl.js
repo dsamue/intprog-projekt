e8d843b454cb3072278a76568238ac723ac286b7
samuelsson87 <samuelsson87@hotmail.com>
2015-03-12 15:05:34 +0100

    Första test - funkar på given kod

--- app/js/sentenceGameCtrl.js
+++ app/js/sentenceGameCtrl.js
@@ -53,4 +53,8 @@ projectApp.controller('SentenceGameCtrl', function ($scope, Model, $routeParams,
     return Model.getMyVar();
   };
 
+  $scope.handleDrop = function(item, bin) {
+    alert('Item ' + item + ' has been dropped into ' + bin);
+  }
+
 });
