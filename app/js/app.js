63774fb12c56f1db167b3a9404c1806d8f7418fa
samuelsson87 <samuelsson87@hotmail.com>
2015-03-13 12:53:54 +0100

    testar med ng-sortable

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,7 @@
 // also see that we included separate JavaScript files for these modules. Angular
 // has other core modules that you might want to use and explore when you go deeper
 // into developing Angular applications. For this lab, these two will suffice.
-var projectApp = angular.module('projectApp', ['ngRoute','ngResource']);
+var projectApp = angular.module('projectApp', ['ngRoute','ngResource','ui.sortable']);
 
 
 // Here we configure our application module and more specifically our $routeProvider. 
--- app/js/mapCtrl.js
+++ app/js/mapCtrl.js
@@ -1,39 +1,10 @@
 // Map controller 
 projectApp.controller('MapCtrl', function ($scope, Model){
 
-	$scope.level = Model.getLevel();
-	
-	//Den här tycker jag känns oklar? Borde inte uppdateringen kunna ske automatiskt när man använder angular?  /David
-	$scope.$watch('level', function(newValue, oldValue)
-		{ if (newValue != oldValue)
-			{
-			moveCharacter();
-			}
-		});
-
-
-//metod som uppdaterar figurens position 
-	$scope.moveCharacter = function(){
-		console.log($scope.level);
-
-		if ($scope.level == 2){
-			$("#character").animate({'left' : "70px"});
-			$("#character").animate({'top' : "200px"});
-		}
-
-		else if ($scope.level == 3){
-			$("#character").animate({'left' : "60px"});
-			$("#character").animate({'top' : "220px"});
-		}
-
-		else if ($scope.level == 4){
-			$("#character").animate({'left' : "40px"});
-			$("#character").animate({'top' : "270px"});
-		}	
-
-		else{
-			$("#character").animate({'left' : "180px"});
-			$("#character").animate({'top' : "80px"});
-		}	
-	};
+$scope.dragControlListeners = {
+    accept: function (sourceItemHandleScope, destSortableScope) {return boolean}//override to determine drag is allowed or not. default is true.
+    itemMoved: function (event) {//Do what you want},
+    orderChanged: function(event) {//Do what you want},
+    containment: '#board'//optional param.
+};
 });
