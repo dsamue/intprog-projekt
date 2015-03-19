8e3f99320f303cc099a195637506c0f8e67c51f0
sann4 <sanna.marklund@gmail.com>
2015-03-19 14:14:00 +0100

    progressbar

--- app/js/progressbarCtrl.js
+++ app/js/progressbarCtrl.js
@@ -1,4 +1,5 @@
-projectApp.controller('progressbarCtrl', function($scope, Model) {
-	$scope.score = Model.getScore();
+projectApp.controller('progressbarCtrl', function ($scope, Model) {
+
+  $scope.score = Model.getScore();
 
 });
